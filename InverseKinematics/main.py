8783a03269e3a30bad5934852c20b426efc553df
MARIANOCEREDA <gmarianocereda@gmail.com>
2022-04-01 22:30:54 +0200

    Instanciacion del robot y llamado a metodos para calculo de primer articulacion

--- InverseKinematics/main.py
+++ InverseKinematics/main.py
@@ -13,17 +13,34 @@ if __name__ == "__main__":
     dhMatrix = np.array([j1,j2,j3,j4,j5,j6])
 
     #T Matrix: Containing the data of the point to reach.
-    T = 
+    f1:list = [0.7959,0.2612,0.5461,0.6795]
+    f2:list = [0.0177,0.8917,-0.4522,0.6699]
+    f3:list = [-0.6051,0.3696,0.7051,2.6586]
+    f4:list = [0,0,0,1]
+    T = np.array([f1,f2,f3,f4])
+
+    #Offset array
+    qOffset = np.array([0,45,45,0,0,0,0])*np.pi/180
 
     #Limits of each joint
-    ql1:list = [185,-185]*np.pi/180
-    ql2:list = [-105,130]*np.pi/180
-    ql3:list = [-165,120]*np.pi/180
-    ql4:list = [-180,180]*np.pi/180
-    ql5:list = [-125,125]*np.pi/180
-    ql6:list = [-350,350]*np.pi/180
-    qlims = np.array([ql1,ql2,ql3,ql4,ql5,ql6])
+    ql1:List[float] = [185,-185]
+    ql2:List[float] = [-105,130]
+    ql3:List[float] = [-165,120]
+    ql4:List[float] = [-180,180]
+    ql5:List[float] = [-125,125]
+    ql6:List[float] = [-350,350]
+    qlims = np.array([ql1,ql2,ql3,ql4,ql5,ql6])*np.pi/180
 
-    R = Robot(dhMatrix,T)
+    #Too matrix
+    toolMatrix = np.identity(4)
+
+    #Base matrix
+    baseMatrix = np.identity(4)
+
+    #Coding of inverse kinematics
+    R = Robot(dhMatrix,qlims,toolMatrix,qOffset,baseMatrix,T)
+    p04 = R.calculatePosition()
+    q1 = R.q1Calculate(p04)
+    
 
 
