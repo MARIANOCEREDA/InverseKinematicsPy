ed2a0cebb8b73a4fca1f1edf0428a69b6826df0d
MARIANOCEREDA <gmarianocereda@gmail.com>
2022-04-01 22:30:14 +0200

    Completado y verificacion del calculo de la primera articulacion

--- InverseKinematics/Robot.py
+++ InverseKinematics/Robot.py
@@ -4,14 +4,14 @@ from typing import List,Dict
 import math
 
 class Robot:
-    def __init__(self,dhMatrix,T) -> None:
+    def __init__(self,dhMatrix,qlims,toolMatrix,qOffset,baseMatrix,T) -> None:
         self.q = list()
         self.dhMatrix = dhMatrix
-        self.qlims = list()
-        self.toolMarix = np.array()
-        self.qoffset = np.array()
+        self.qlims = qlims
+        self.toolMarix = toolMatrix
+        self.qoffset = qOffset
         self.qlast = np.zeros((6))
-        self.baseMatrix = np.array()
+        self.baseMatrix = baseMatrix
         self.qfinal = np.zeros((6,8))
         self.T = T
     
@@ -25,12 +25,12 @@ class Robot:
         p04:np.array = self.T[:3,3]- self.dhMatrix[5,1]* self.T[:3,3]
         return p04
 
-    def q1Calculate(self,p04:List[int]) -> np.array:
+    def q1Calculate(self,p04) -> np.array:
         '''
         It calculates the possible values for the first joint. In this case, we have 2 possible values.
         '''
         q1 = np.zeros(2)
-        q1[0] = math.atan2(p04(1),p04(0))
+        q1[0] = math.atan2(p04[1],p04[0])
 
         #Depending on the value of q1[0], the value of q1[1] will change
         if(q1[0]>0):
@@ -41,3 +41,10 @@ class Robot:
         #Now we fill the matrix qFinal 
         self.qfinal[0,:3] = q1[0]
         self.qfinal[0,4:7] = q1[1]
+        return q1
+    
+    def q2Calculate(self,q1):
+        '''
+        It calculates 4 possible values for the second joint. They depend on the q1 value.
+        '''
+        
